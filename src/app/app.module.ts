36e78fe28e66dcb6f4f8e89e4e677e31c947e995
rudrasantadip <santadip9@gmail.com>
2024-10-09 07:56:34 +0530

    initialized nextyojona

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
